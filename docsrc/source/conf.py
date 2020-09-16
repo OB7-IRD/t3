a0a624dd3fa35493067137417e1c1ef901be54e1
Julien Lebranchu <julien.lebranchu@ird.fr>
2020-09-16 11:29:04 +0200

    Add markdown format to sphinx

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -32,6 +32,7 @@ release = 'v3.0'
 extensions = [
     "sphinx.ext.githubpages",
     'recommonmark',
+    'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -69,3 +70,14 @@ pygments_style = 'monokai'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+
+autosectionlabel_prefix_document = True
+from recommonmark.transform import AutoStructify
+
+github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
