d00512b367d6ec8359f4da1b25bd445835b54f87
Mathieu DEPETRIS <35579752+MathieuDepetris@users.noreply.github.com>
2021-09-01 15:28:10 +0200

    Add class "standardisedsamplesets"

--- R/standardisedsamplesets.R
+++ R/standardisedsamplesets.R
@@ -1,13 +1,13 @@
-#' @name elementarylandings
-#' @title R6 class elementarylandings
-#' @description Create R6 reference object class elementarylandings
+#' @name standardisedsamplesets
+#' @title R6 class standardisedsamplesets
+#' @description Create R6 reference object class standardisedsamplesets
 #' @importFrom R6 R6Class
-elementarylandings <- R6::R6Class(classname = "elementarylandings",
+standardisedsamplesets <- R6::R6Class(classname = "standardisedsamplesets",
                                   inherit = t3:::list_t3,
                                   public = list(
                                     # initialize ----
-                                    #' @description Initialize function for R6 elementarylandings class.
-                                    #' @param ... (empty, list or R6-elementarylanding classes) Nothing, a list of object R6-elementarylanding classes or one object R6-elementarylanding classes.
+                                    #' @description Initialize function for R6 standardisedsamplesets class.
+                                    #' @param ... (empty, list or R6-standardisedsampleset classes) Nothing, a list of object R6-standardisedsampleset classes or one object R6-standardisedsampleset classes.
                                     initialize = function(...) {
                                       arguments <- list(...)
                                       if (nargs() == 0) {
@@ -16,59 +16,59 @@ elementarylandings <- R6::R6Class(classname = "elementarylandings",
                                         for (i in 1:nargs()) {
                                           if (length(class(arguments[[i]])) == 1 && class(arguments[[i]]) == "list") {
                                             for (i in length(arguments[[i]])) {
-                                              if (length(class(arguments[[i]][[i]])) == 2 && (! any(class(arguments[[i]][[i]]) == "R6") & ! any(class(new_item[[i]]) == "elementarylanding"))) {
+                                              if (length(class(arguments[[i]][[i]])) == 2 && (! any(class(arguments[[i]][[i]]) == "R6") & ! any(class(new_item[[i]]) == "standardisedsampleset"))) {
                                                 cat(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),
-                                                    " - Error: invalid \"data\" argument, class list or R6-elementarylanding expected.\n",
+                                                    " - Error: invalid \"data\" argument, class list or R6-standardisedsampleset expected.\n",
                                                     sep = "")
                                                 stop()
                                               }
                                             }
                                             private$data <- append(private$data, arguments[[i]])
-                                          } else if (length(class(arguments[[i]])) == 2 && (any(class(arguments[[i]]) == "R6") & any(class(arguments[[i]]) == "elementarylanding"))) {
+                                          } else if (length(class(arguments[[i]])) == 2 && (any(class(arguments[[i]]) == "R6") & any(class(arguments[[i]]) == "standardisedsampleset"))) {
                                             private$data <- append(private$data, arguments[[i]])
                                           } else {
                                             cat(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),
-                                                " - Error: invalid \"data\" argument, class list or R6-elementarylanding expected.\n",
+                                                " - Error: invalid \"data\" argument, class list or R6-standardisedsampleset expected.\n",
                                                 sep = "")
                                             stop()
                                           }
                                         }
                                       }
                                     },
-                                    # add new elementarylanding ----
-                                    #' @description Function for add a new elementarylanding in the object elementarylandings.
-                                    #' @param new_item (list or R6-elementarylanding classes) A list of object R6-elementarylanding classes or one object R6-elementarylanding classes.
+                                    # add new standardisedsampleset ----
+                                    #' @description Function for add a new standardisedsampleset in the object standardisedsamplesets.
+                                    #' @param new_item (list or R6-standardisedsampleset classes) A list of object R6-standardisedsampleset classes or one object R6-standardisedsampleset classes.
                                     add = function(new_item) {
                                       if (length(class(new_item)) == 1 && class(new_item) == "list") {
                                         for (i in length(new_item)) {
-                                          if (length(class(new_item[[i]])) == 2 && (! any(class(new_item[[i]]) == "R6") & ! any(class(new_item[[i]]) == "elementarylanding"))) {
+                                          if (length(class(new_item[[i]])) == 2 && (! any(class(new_item[[i]]) == "R6") & ! any(class(new_item[[i]]) == "standardisedsampleset"))) {
                                             cat(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),
-                                                " - Error: invalid \"data\" argument, class list or R6-elementarylanding expected.\n",
+                                                " - Error: invalid \"data\" argument, class list or R6-standardisedsampleset expected.\n",
                                                 sep = "")
                                             stop()
                                           }
                                         }
                                         super$add(new_item)
-                                      } else if (length(class(new_item)) == 2 && (any(class(new_item) == "R6") & any(class(new_item) == "elementarylanding"))) {
+                                      } else if (length(class(new_item)) == 2 && (any(class(new_item) == "R6") & any(class(new_item) == "standardisedsampleset"))) {
                                         super$add(new_item)
                                       } else {
                                         cat(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),
-                                            " - Error: invalid \"data\" argument, class list or R6-elementarylanding expected.\n",
+                                            " - Error: invalid \"data\" argument, class list or R6-standardisedsampleset expected.\n",
                                             sep = "")
                                         stop()
                                       }
                                     },
                                     # filter by trip ----
-                                    #' @description Function for filter elementarylandings by trip identification.
+                                    #' @description Function for filter standardisedsamplesets by trip identification.
                                     #' @param trip_id (character) Trip identification.
                                     filter_by_trip = function(trip_id) {
-                                      current_elementarylandings <- vector(mode = "list")
+                                      current_standardisedsamplesets <- vector(mode = "list")
                                       for (i in seq_len(length(private[["data"]]))) {
                                         current_trip_id <- private[["data"]][[i]]$.__enclos_env__$private$trip_id
                                         if (trip_id == current_trip_id) {
-                                          current_elementarylandings <- append(current_elementarylandings,
+                                          current_standardisedsamplesets <- append(current_standardisedsamplesets,
                                                                                list(private[["data"]][[i]]))
                                         }
                                       }
-                                      return(current_elementarylandings)
+                                      return(current_standardisedsamplesets)
                                     }))
