42821de559b6c70719354b9a9724541e3cb57325
Antoine Duparc <antoine.duparc@ird.fr>
2022-03-29 16:16:22 +0200

    Review of process 3.1
    Minor change in the parameter description. The process 3.1 is working.

--- R/full_trips.R
+++ R/full_trips.R
@@ -3507,17 +3507,17 @@ full_trips <- R6::R6Class(classname = "full_trips",
                             },
                             # process 3.1: data preparatory ----
                             #' @description Data preparatory for the t3 modelling process (level 3).
-                            #' @param inputs_level3 Object of type \code{\link[base]{data.frame}} expected. Imputs of levels 3 (see function path to level 3).
-                            #' @param inputs_level3_path Object of type \code{\link[base]{character}} expected. Path to the folder containing yearly data ouptut of the level 1 and 2.
+                            #' @param inputs_level3 Object of type \code{\link[base]{data.frame}} expected. Inputs of levels 3 (see function path to level 3).
+                            #' @param inputs_level3_path Object of type \code{\link[base]{character}} expected. Path to the folder containing yearly data output of the level 1 and 2 (output of the function the path to level 3). If provide, replace the inputs_level3 object.
                             #' @param outputs_directory Object of type \code{\link[base]{character}} expected. Path of the t3 processes outputs directory.
                             #' @param periode_reference Object of type \code{\link[base]{integer}} expected. Year(s) period of reference for modelling estimation.
-                            #' @param target_year Object of type \code{\link[base]{integer}} expected. Year of interest for the model estimaton and prediction.Default value is current year -1.
-                            #' @param period_duration Object of type \code{\link[base]{integer}} expected. number of year use for the modelling. The default value is 5
+                            #' @param target_year Object of type \code{\link[base]{integer}} expected. Year of interest for the model estimation and prediction.Default value is current year -1.
+                            #' @param period_duration Object of type \code{\link[base]{integer}} expected. number of years use for the modelling. The default value is 5
                             #' @param distance_maximum Object of type \code{\link[base]{integer}} expected. Maximum distance between all sets of a sampled well. By default 5.
                             #' @param number_sets_maximum Object of type \code{\link[base]{integer}} expected. Maximum number of sets allowed in mixture. By default 5.
-                            #' @param set_weight_minimum Object of type \code{\link[base]{integer}} expected. Minimum set size considered. Remove smallest set for which sample could not be representative. By default 6.
-                            #' @param minimum_set_frequency Object of type \code{\link[base]{numeric}} expected. Minimum freqency that a set could represent in a well. Another filter considering other set size in the well. By default 0.1.
-                            #' @param vessel_id_ignored Object of type \code{\link[base]{integer}} expected. Specify here vessel(s) id(s) if you whant to ignore it in the model estimation and prediction .By default NULL.
+                            #' @param set_weight_minimum Object of type \code{\link[base]{integer}} expected. Minimum set size considered. Remove smallest set for which sample could not be representative. By default 6 t.
+                            #' @param minimum_set_frequency Object of type \code{\link[base]{numeric}} expected. Minimum threshold proportion of set in awell to be used for model training in the process. By default 0.1.
+                            #' @param vessel_id_ignored Object of type \code{\link[base]{integer}} expected. Specify list of vessel(s) id(s) to be ignored in the model estimation and prediction .By default NULL.
                             data_preparatory = function(inputs_level3 = NULL,
                                                         inputs_level3_path = NULL,
                                                         outputs_directory,
@@ -3739,7 +3739,7 @@ full_trips <- R6::R6Class(classname = "full_trips",
                                 sample_set_char <- list(sset = sset,
                                                         act_chr = act_chr,
                                                         catch_set_lb = catch_set_lb)
-                                # compute set weight in each sample to detect non representivness of the sample
+                                # compute set weight in each sample to detect non representiveness of the sample
                                 agg_wp <- aggregate(formula = cbind(w_in_well = weight) ~ id_sample + id_well + id_act,
                                                     data = wp,
                                                     FUN = sum)
@@ -3754,7 +3754,7 @@ full_trips <- R6::R6Class(classname = "full_trips",
                                 # selection based on sets extrapolated (2 first step of the t3 process)
                                 kiset <- sset2
                                 # on sample
-                                # homogneous fishing mode in sample
+                                # homogeneous fishing mode in sample
                                 kiset <- kiset[kiset$fm_purity == 1, ]
                                 # spatial selection + mixture limit
                                 kiset <- kiset[kiset$lat_sample_dif < distance_maximum & kiset$lon_sample_dif < distance_maximum & kiset$nset < number_sets_maximum, ]
