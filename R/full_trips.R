72879f6e128f7c5fca871c87d882df47e68e093a
Jeanne CLEMENT <jeanne.clement@ird.fr>
2024-10-18 19:49:30 +0200

    Add code 14 (= >10kg) for YFT, BET & ALB in IO

--- R/full_trips.R
+++ R/full_trips.R
@@ -1586,7 +1586,7 @@ full_trips <- R6::R6Class(classname = "full_trips",
                                                             current_elementarycatch_tmp$.__enclos_env__$private$catch_weight_category_code_corrected <- current_elementarycatch_tmp$.__enclos_env__$private$catch_weight_rf2 * 0.8
                                                             private$data_selected[[full_trip_id]][[trip_id]]$.__enclos_env__$private$activities[[activity_id]]$.__enclos_env__$private$elementarycatches <- append(private$data_selected[[full_trip_id]][[trip_id]]$.__enclos_env__$private$activities[[activity_id]]$.__enclos_env__$private$elementarycatches,
                                                                                                                                                                                                                    current_elementarycatch_tmp)
-                                                          } else if (current_weight_category_code %in% c(3, 12, 5, 7, 8, 13, 6, 11)) {
+                                                          } else if (current_weight_category_code %in% c(3, 12, 5, 7, 8, 13, 14, 6, 11)) {
                                                             current_elementarycatch$.__enclos_env__$private$weight_category_code_corrected <- category_4
                                                             current_elementarycatch$.__enclos_env__$private$catch_weight_category_code_corrected <- current_elementarycatch$.__enclos_env__$private$catch_weight_rf2
                                                           } else if (current_weight_category_code == 9) {
