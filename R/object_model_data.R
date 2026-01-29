c2c6e0e643ad8e4ad6be1072a52d04bcc5e44a27
jeanne.clement <jeanne.clement@ird.fr>
2026-01-29 15:56:30 +0100

    Add trips_data %>% dplyr::arrange(vessel_code, trip_end_date) to avoid not full trips because of trips'order after double query

--- R/object_model_data.R
+++ R/object_model_data.R
@@ -315,6 +315,8 @@ object_model_data <- R6::R6Class(classname = "object_model_data",
                                            " - Successful trip(s) data importation R environment.\n", sep="")
                                      }
                                      # 7 - Common data design ----
+                                     trip_data <- trip_data %>%
+                                       dplyr::arrange(vessel_code, trip_end_date)
                                      trip_data <- unclass(x = trip_data)
                                      object_trips <- object_r6(class_name = "trips")
                                      T1 <- Sys.time()
